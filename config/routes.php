b68bb1f9b9a7258d016cd5f8959e1e981bda5eee
andreea_s <andreea.stoenescu@hotmail.com>
2018-01-23 15:10:50 +0000

    add users entity and /api route prefix

--- config/routes.php
+++ config/routes.php
@@ -46,6 +46,9 @@ Router::defaultRouteClass(DashedRoute::class);
 Router::prefix('api', function ($routes) {
     $routes->extensions(['json', 'xml']);
     $routes->resources('Pets');
+    $routes->resources('Users');
+    Router::connect('/api/users/register', ['controller' => 'Users', 'action' => 'add', 'prefix' => 'api']);
+    $routes->fallbacks('InflectedRoute');
 });
 
 Router::scope('/', function (RouteBuilder $routes) {
